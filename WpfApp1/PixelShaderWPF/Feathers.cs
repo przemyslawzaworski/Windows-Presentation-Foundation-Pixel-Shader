cf7db4e3064cee9577260bf7986880a8293c9072
Przemyslaw Zaworski <przemyslawzaworski@gmail.com>
2019-05-14 22:48:56 +0200

    Update 1

---
+++ WpfApp1/PixelShaderWPF/Feathers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Media.Effects;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PixelShaderWPF
+{
+    public class Feathers : ShaderEffect
+    {
+        public static readonly DependencyProperty InputProperty = ShaderEffect.RegisterPixelShaderSamplerProperty("Input", typeof(Feathers), 0);
+
+        public Brush Input
+        {
+            get => ((Brush)(GetValue(InputProperty)));
+            set => SetValue(InputProperty, value);
+        }
+
+        public Feathers()
+        {
+            PixelShader pixelShader = new PixelShader { UriSource = new Uri(@"pack://application:,,,/PixelShaderWPF;component/feathers.ps") };
+            PixelShader = pixelShader;
+            UpdateShaderValue(InputProperty);
+            UpdateShaderValue(TimeProperty);
+        }
+
+        public static readonly DependencyProperty TimeProperty = DependencyProperty.Register("time", typeof(double), typeof(Feathers), new PropertyMetadata(0.0, PixelShaderConstantCallback(0)));
+
+        public double Timer
+        {
+            get => ((double)(this.GetValue(TimeProperty)));          
+            set => this.SetValue(TimeProperty, value);            
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+using System;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer dt = new DispatcherTimer();
+            dt.Tick += new EventHandler(Tick);
+            dt.Interval = new System.TimeSpan(0,0,0,0,1); 
+            dt.Start();
+        }
+  
+        private void Tick(object sender, EventArgs e)
+        {
+            feathers.Timer = System.Environment.TickCount * 0.001f;
+        }
+    }
+}
